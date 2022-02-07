eeb59d01da34d9b7adf6f5df5bef2f533e7313a0
mcisne4 <48368011+mcisne4@users.noreply.github.com>
2022-02-07 03:04:44 -0600

    Add ScoreKeeper deply script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ScoreKeeper = await hre.ethers.getContractFactory("ScoreKeeper");
+  const scoreKeeper = await ScoreKeeper.deploy();
 
-  await greeter.deployed();
+  await scoreKeeper.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("ScoreKeeper deployed to:", scoreKeeper.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
